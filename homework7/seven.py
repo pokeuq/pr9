8ee4bbc92d59d6d1cb00c3d5c34b0a97e655a977
pokeuq <109028724+pokeuq@users.noreply.github.com>
2024-10-12 13:30:13 +0500

    7 задание

--- homework7/seven.py
+++ homework7/seven.py
@@ -1,21 +1,4 @@
 def shift_right(numbers):
-  if len(numbers) > 0:
-      last_element = numbers[-1]
-      for i in range(len(numbers) - 1, 0, -1):
-          numbers[i] = numbers[i - 1]
-      numbers[0] = last_element
-  print(f"Циклически сдвинутый список: {numbers}")
-
-if __name__ == "__main__":
-  numbers = input("Введите список чисел через пробел: ").split()
-
-  try:
-      numbers = [int(x) for x in numbers]
-  except ValueError:
-      print("Ошибка: Введите только целые числа.")
-  else:
-      shift_right(numbers)
-def shift_right(numbers):
     if len(numbers) == 0:
         print("Список пустой.")
         return
